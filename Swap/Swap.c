afa8179982e3e60c392ec03645a57f25ff99f856
utnso <utnso@utnso40>
2016-04-23 12:43:04 -0300

    Pusheo parte del swap que falta

--- Swap/Swap.c
+++ Swap/Swap.c
@@ -1,2 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "commons/config.h"
+
+struct bloqueDeMemoria{
+	int idBloque;
+	int ocupado;
+
+}typedef bloqueSwap;
+
+main(){
+	system("dd ");
+	bloqueSwap roberto;
+	roberto.ocupado=0;
+
+
+}
 
 
