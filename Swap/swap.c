d6853586a8d259cddd06996eae902b9edb02e9df
leofs94 <leofs_94@hotmail.com>
2016-06-09 03:06:27 -0300

    Pusheo config ya esta la conexion hecha listo para probar y debuguear por completo

--- Swap/swap.c
+++ Swap/swap.c
@@ -1,4 +1,7 @@
-
+#define agregar_proceso 1
+#define finalizar_proceso 2
+#define leer_pagina 3
+#define escribir_pagina 4
 #include <stdio.h>
 #include <stdlib.h>
 #include "commons/collections/list.h"
@@ -6,6 +9,8 @@
 #include "commons/string.h"
 #include <string.h>
 #include <sys/mman.h>
+#include "commons/config.h"
+#include "sockets.h"
 
 struct bloqueDeMemoria{
 	int PID;
@@ -15,50 +20,106 @@ struct bloqueDeMemoria{
 	int paginaInicial;
 
 }typedef bloqueSwap;
+/*<----------------------------------Prototipos----------------------------------------->*/
+ void* mapearArchivoEnMemoria(int offset,int tamanio);
+ void destructorBloqueSwap(bloqueSwap* self);
+ int agregarProceso(bloqueSwap* unBloque,t_list* unaLista);
+ int compactarArchivo(t_list* unaLista);
+ void crearArchivoDeSwap();
+ void escribirPagina(char* paginaAEscribir,bloqueSwap* unBloque,t_list* listaSwap);
+ int verificarEspacioDisponible(t_list* listaSwap);
+ void leerPagina(bloqueSwap* bloqueDeSwap,t_list* listaSwap);
+ void crearArchivoDeConfiguracion();
+
+/*>----------------------------------Prototipos-----------------------------------------<*/
+
+
 
 ///////////////////////configurable trucho//////////////////////////////////
-int puertoEscucha;
+int* puertoEscucha;
 int tamanioDePagina;
 int cantidadDePaginas;
 int retardoCompactacion;
-char nombre_swap[50];
+int retardoAcceso;
+char* nombre_swap;
 t_list* listaSwap;
 
 int main(){
-listaSwap=list_create();
+crearArchivoDeConfiguracion();
+crearArchivoDeSwap();
+FILE* archivoSwap;
+char* paginaAEnviar;
 bloqueSwap* bloqueInicial;
+char* paginaRecibida;
+int* socketUMC;
+bloqueSwap* pedidoRecibidoYDeserializado;
+char* mensajeRecibido;
+char* operacionRecibida;
 bloqueInicial->PID=0;
 bloqueInicial->ocupado=0;
 bloqueInicial->tamanioDelBloque=cantidadDePaginas*tamanioDePagina;
-
-
 list_add(listaSwap,(void*)bloqueInicial);
+openServerConnection((int)socketUMC,puertoEscucha);
+sendClientHandShake(socketUMC,SWAP);
+
+while(1){
+		receiveMessage(socketUMC,operacionRecibida,sizeof(bloqueSwap));
+		//deserializarOperacion(operacionARealizar,operacionRecibida);
+
+		receiveMessage(socketUMC,mensajeRecibido,sizeof(bloqueSwap));
+		//deserializarBloqueSwap(pedidoRecibidoYDeserializado,bloqueRecibido)
+		switch(pedidoRecibidoYDeserializado->PID){
+		case agregar_proceso:{
+		if(verificarEspacioDisponible(listaSwap)>pedidoRecibidoYDeserializado->cantDePaginas){
+		if(existeBloqueNecesitado(listaSwap)){
+			agregarProceso(pedidoRecibidoYDeserializado,listaSwap);
+		}else{
+			compactarArchivo(listaSwap);
+			agregarProceso(pedidoRecibidoYDeserializado,listaSwap);
+		}
+
+	}}
+	break;
+case finalizar_proceso:{
+	eliminarProceso(listaSwap,pedidoRecibidoYDeserializado);
+	break;
+	}
+
+case leer_pagina:{
+	leerPagina(pedidoRecibidoYDeserializado,listaSwap);
+	break;
+	}
+case escribir_pagina:{
+	escribirPagina(paginaRecibida,pedidoRecibidoYDeserializado,listaSwap);
+	break;
+	}
+
+}
+
+}
 
 return 1;
 
 }
 
+void destructorBloqueSwap(bloqueSwap* self){
+	free(self->PID);
+	free(self->cantDePaginas);
+	free(self->ocupado);
+	free(self->paginaInicial);
+	free(self->tamanioDelBloque);
+	free(self);
+}
+
 int agregarProceso(bloqueSwap* unBloque,t_list* unaLista){
 	/*<----------------------------------------------abroElArchivo----------------------------------------------------->*/
-	FILE *archivoSwap;
-	int descriptorSwap;
 	void* archivoMapeado;
-	archivoSwap=fopen(nombre_swap,"a+");
-	descriptorSwap=fileno(archivoSwap);
-	lseek(descriptorSwap,0,SEEK_SET);
-	archivoMapeado=mmap(0,(tamanioDePagina*cantidadDePaginas),PROT_WRITE,MAP_SHARED,(void*)descriptorSwap,(tamanioDePagina*unBloque->paginaInicial));
+	archivoMapeado=mapearArchivoEnMemoria(tamanioDePagina*unBloque->paginaInicial,cantidadDePaginas*tamanioDePagina);
 	bloqueSwap* elementoEncontrado;
 	elementoEncontrado=(bloqueSwap*)malloc(sizeof(bloqueSwap));
 	bloqueSwap* nuevoBloqueVacio;
 	nuevoBloqueVacio=(bloqueSwap*)malloc(sizeof(bloqueSwap));
-	void destructorBloqueSwap(bloqueSwap* self){
-		free(self->PID);
-		free(self->cantDePaginas);
-		free(self->ocupado);
-		free(self->paginaInicial);
-		free(self->tamanioDelBloque);
-		free(self);
-	}
+
 
 	bool posibleBloqueParaLlenar(bloqueSwap* unBloque,bloqueSwap* posibleBloque,void* destructorBloqueSwap){
 			return (unBloque->ocupado==0 && unBloque->cantDePaginas<=posibleBloque->cantDePaginas);
@@ -81,27 +142,16 @@ int agregarProceso(bloqueSwap* unBloque,t_list* unaLista){
 	return 0;
 }
 
-void destructorBloqueSwap(bloqueSwap* self){
-	free(self->PID);
-	free(self->cantDePaginas);
-	free(self->ocupado);
-	free(self->paginaInicial);
-	free(self->tamanioDelBloque);
-	free(self);
-}
+
 
 int compactarArchivo(t_list* unaLista){
 	int i,acum;
 	bloqueSwap* bloqueLleno=malloc(sizeof(bloqueSwap));
 	bloqueSwap* bloqueLlenoSiguiente=malloc(sizeof(bloqueSwap));
 	bloqueSwap* bloqueVacioCompacto=malloc(sizeof(bloqueSwap));
-
 	int bloqueVacioAEliminar(bloqueSwap unBloque){
 		return (unBloque.ocupado==0);
-	};
-
-
-
+		};
 	for(i=0;unaLista->elements_count<=i;i++){
 		{	list_remove_and_destroy_by_condition(unaLista,(void*)bloqueVacioAEliminar,(void*)destructorBloqueSwap);
 		}
@@ -113,17 +163,16 @@ int compactarArchivo(t_list* unaLista){
 		bloqueLlenoSiguiente=(bloqueSwap*)list_get(unaLista,i+1);
 		bloqueLlenoSiguiente->paginaInicial=bloqueLleno->cantDePaginas+1;
 		acum+=bloqueLlenoSiguiente->cantDePaginas;
-	}
+		}
 		if(cantidadDePaginas-acum!=0){
 		bloqueVacioCompacto->cantDePaginas=cantidadDePaginas-acum;
 		bloqueVacioCompacto->ocupado=0;
 		bloqueVacioCompacto->PID=0;
 		bloqueVacioCompacto->paginaInicial=acum+1;
-
 		list_add(unaLista,bloqueVacioCompacto);
+		}
 		return 0;
-	}
-}}
+	}}
 
 int eliminarProceso(t_list* unaLista,bloqueSwap unProceso){
 	bloqueSwap* procesoAEliminar=malloc(sizeof(bloqueSwap));
@@ -135,58 +184,94 @@ int eliminarProceso(t_list* unaLista,bloqueSwap unProceso){
 }
 
 void crearArchivoDeSwap(){
-	char primerParteCadena[50]="dd if=dev/zero of=";
-	char* tamanioPagina;
-	tamanioPagina=string_itoa(tamanioDePagina);
-	char* cadCantidadDePaginas;
-	cadCantidadDePaginas=string_itoa(cantidadDePaginas);
-	char segundaParteCadena[5]=" bs=";
-	char terceraParteCadena[10]=" count=";
-	char* cadenaTotal;
-	cadenaTotal=strcat(primerParteCadena,nombre_swap);
-	cadenaTotal=strcat(cadenaTotal,segundaParteCadena);
-	cadenaTotal=strcat(cadenaTotal,tamanioPagina);
-	cadenaTotal=strcat(cadenaTotal,terceraParteCadena);
-	cadenaTotal=strcat(cadenaTotal,cadCantidadDePaginas);
-	system(cadenaTotal[50]);
+	int tamanioDePagina;
+			tamanioDePagina=256;
+			int cantidadDePaginas;
+			cantidadDePaginas=10;
+
+			char* cadena=string_new();
+			string_append(&cadena,"dd if=/dev/zero of=/home/utnso/Escritorio/");
+
+
+			string_append(&cadena,nombre_swap);
+			char* segundaParteCadena=string_new();
+			string_append(&segundaParteCadena," bs=");
+			string_append(&cadena,segundaParteCadena);
+			string_append_with_format(&cadena,"%i",tamanioDePagina);
+			char* terceraParteCadena=string_new();
+			string_append(&terceraParteCadena," count=");
+			string_append(&cadena,terceraParteCadena);
+			string_append_with_format(&cadena,"%i",cantidadDePaginas);
+			system(cadena);
 	}
 int condicionLeer(bloqueSwap* unBloque,bloqueSwap* otroBloque){
 			return (unBloque->PID==otroBloque->PID);
 			}
 void leerPagina(bloqueSwap* bloqueDeSwap,t_list* listaSwap){
-
-
 	bloqueSwap* bloqueEncontrado;
-	FILE* archivoSwap;
-	int descriptorSwap;
-	char* archivoMapeado;
+	void* archivoMapeado;
 	char* paginaAEnviar;
-	archivoSwap=fopen(nombre_swap,"a+");
-	descriptorSwap=fileno(archivoSwap);
-	lseek(descriptorSwap,0,SEEK_SET);
 	bloqueEncontrado=list_find(listaSwap,(void*)condicionLeer);
-	archivoMapeado=mmap(0,tamanioDePagina*cantidadDePaginas,PROT_WRITE,MAP_SHARED,descriptorSwap,tamanioDePagina*bloqueDeSwap->paginaInicial);
+	archivoMapeado=mapearArchivoEnMemoria(tamanioDePagina*bloqueDeSwap->paginaInicial,tamanioDePagina*cantidadDePaginas);
 	memcpy(paginaAEnviar,archivoMapeado,tamanioDePagina);
+	munmap(archivoMapeado,cantidadDePaginas*tamanioDePagina);
 	//enviar(paginaAEnviar);
 	}
 
 void escribirPagina(char* paginaAEscribir,bloqueSwap* unBloque,t_list* listaSwap){
 	int descriptorSwap;
-	char* archivoMapeado;
-	FILE* archivoSwap;
-	archivoSwap=fopen(nombre_swap,"a+");
-	descriptorSwap=fileno(archivoSwap);
-	lseek(descriptorSwap,0,SEEK_SET);
-
-	archivoMapeado=mmap(0,tamanioDePagina*cantidadDePaginas,PROT_WRITE,MAP_SHARED,descriptorSwap,tamanioDePagina*unBloque->paginaInicial);
+	void* archivoMapeado;
+	archivoMapeado=mapearArchivoEnMemoria(tamanioDePagina*unBloque->paginaInicial,tamanioDePagina*cantidadDePaginas);
 	memset(archivoMapeado+unBloque->cantDePaginas,paginaAEscribir,tamanioDePagina);
+	munmap(archivoMapeado,cantidadDePaginas*tamanioDePagina);
 }
 
 
+void* mapearArchivoEnMemoria(int offset,int tamanio){
+	FILE* archivoSwap;
+	int descriptorSwap;
+	void* archivoMapeado;
+	archivoSwap=fopen(nombre_swap,"a+");
+	descriptorSwap=fileno(archivoSwap);
+	lseek(descriptorSwap,0,SEEK_SET);
+	archivoMapeado=mmap(0,tamanio,PROT_WRITE,MAP_SHARED,descriptorSwap,offset);
+	return archivoMapeado;
 
+}
 
+int elementosVacios(bloqueSwap* unElemento){
+	return unElemento->ocupado==0;
+}
+int verificarEspacioDisponible(t_list* listaSwap){
+	t_list* listaFiltrada;
+	int i;
+	int acum;
+	bloqueSwap* bloqueDevuelto;
+	listaFiltrada=list_filter(listaSwap,(void*)elementosVacios);
+	for(i=0;i<listaFiltrada->elements_count;i++){
+		bloqueDevuelto=list_get(listaFiltrada,i);
+		acum=acum+(bloqueDevuelto->cantDePaginas);
 
+	}
+	return acum;
+}
 
+int condicionDeCompactacion(bloqueSwap* unBloque,bloqueSwap* otroBloque){
+	return(unBloque->cantDePaginas>=otroBloque->cantDePaginas);
+}
 
+int existeElBloqueNecesitado(t_list* listaSwap){
+	return list_any_satisfy(listaSwap,(void*)condicionDeCompactacion);
+}
 
+void crearArchivoDeConfiguracion(){
+	t_config* configuracionDeSwap;
+	configuracionDeSwap=config_create("/home/utnso/git/tp-2016-1c-YoNoFui/Swap/Config.swap");
+	puertoEscucha=config_get_int_value(configuracionDeSwap,"PUERTO_ESCUCHA");
+	nombre_swap=config_get_string_value(configuracionDeSwap,"NOMBRE_SWAP");
+	retardoCompactacion=config_get_int_value(configuracionDeSwap,"RETARDO_COMPACTACION");
+	retardoAcceso=config_get_int_value(configuracionDeSwap,"RETARDO_ACCESO");
+	tamanioDePagina=config_get_int_value(configuracionDeSwap,"TAMANIO_PAGINA");
+	cantidadDePaginas=config_get_int_value(configuracionDeSwap,"CANTIDAD_PAGINAS");
+}
 
